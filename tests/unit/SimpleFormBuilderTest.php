c5ddc6324d04ae04a3325aa0904e9aa59331d00e
gadi-promienna <antrbarbara@webdeveloper@gmail.com>
2025-05-26 16:05:14 +0200

    Add test to check if SimpleFormBuilder correct sets fields params

--- tests/unit/SimpleFormBuilderTest.php
+++ tests/unit/SimpleFormBuilderTest.php
@@ -16,13 +16,21 @@ class SimpleFormBuilderTest extends TestCase
 {
 
     /**
+     * Set up the test environment
+     */
+    public function setUp(): void
+    {
+        // Mock the wp_nonce_field function
+        Monkey\Functions\when('wp_nonce_field')->justReturn('mock_nonce');
+    }
+
+    /**
      * Test Function to check if simple form builder generates correct Simple Form
      */
     public function test_simple_form_builder_create_correct_form()
     {
-        
-     Monkey\Functions\when('wp_nonce_field')->justReturn('mock_nonce');
-     ob_start();
+     $this->setUp();
+     ob_start(); // Start output buffering
      $form_builder = new \FormCreator\SimpleFormBuilder();
      $form_builder->setConfiguration([
         "action" => "/form.php",
@@ -36,5 +44,28 @@ class SimpleFormBuilderTest extends TestCase
     $this->assertStringContainsString('<input type="text"', $output);
     $this->assertStringContainsString('<input type="email"', $output);
     $this->assertStringContainsString('<textarea', $output);    
+ }
+
+    /**
+     * Test Function to check if simple form builder generates correct Simple Form
+     */
+    public function test_simple_form_builder_add_correct_field_params()
+    {
+       $this->setUp();
+        ob_start(); // Start output buffering
+        $form_builder = new \FormCreator\SimpleFormBuilder();
+        $form_builder->setConfiguration([
+            "action" => "/form.php",
+        ]);   
+    $form_builder->addTextField("name", "Imię", ["required"=> true,"class"=> "name"]);
+    $form_builder->addEmailField("email", "Email", ["required"=> true]);
+    $form_builder->addTextareaField("description", "Opis", ["required"=> true, "class"=> "description"]);
+    $test_form2 = $form_builder->build();
+    $test_form2->render();
+    $output = ob_get_clean();
+    $required_count = substr_count($output, 'required'); 
+    $this->assertEquals(3, $required_count, 'Formularz powinien mieć 3 pola wymagane');// Check if all fields are required
+    $this->assertStringContainsString('class="name"', $output);
+    $this->assertStringContainsString('class="description"', $output);    
  }
 }
